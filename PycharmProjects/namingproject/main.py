11b8669d725f5b5ece3e5e0446a2dfd6421d5ab4
naviprasaad50@gmai.com <naviprasaad50@gmail.com>
2024-07-20 14:54:43 +0530

    baby_names

--- PycharmProjects/namingproject/main.py
+++ PycharmProjects/namingproject/main.py
@@ -1,5 +1,5 @@
 import mysql.connector
-
+import tabulate
 connections = mysql.connector.connect(
     host="localhost",
     user="naveen",
@@ -17,13 +17,14 @@ mn = {
 print("Note:")
 print("you can choose one or more choices but To view all tha name you cannot choose other choices")
 print("\n")
-choice = int(input("1.starting_name\n2.ending_name\n3.category\n4.length\n5.gender\n6. All names\nEnter your choice:\t"))
+choice = int(input("1.starting_name\n2.ending_name\n3.category\n4.length\n5.gender\nEnter your choice:\t"))
 choices = []
 for i in str(choice):
     if i not in choices:
         choices.append(int(i))
 if 1 in choices:
-   mn['starting_name']  = input("Enter the starting letter : ").upper()
+   mn['starting_name']\
+       = input("Enter the starting letter : ").upper()
 if 2 in choices:
     mn['ending_name'] = input("Enter the ending letter : ").lower()
 if 4 in choices:
@@ -57,27 +58,23 @@ if 5 in choices:
             continue
 m = ["name like '{}%'".format(str(mn['starting_name'])), "name like '%{}'".format(str(mn['ending_name'])), "category = '{}'".format(str(mn['Category']))
     , "char_length(name)={}".format(mn['length']),"gender = '{}'".format(str(mn['gender']))]
+
 cursor = connections.cursor()
-if 6 in choices:
-    cursor.execute("select name from names")
-    names = cursor.fetchall()
-    connections.commit()
-    for i in names:
-        print(i[0])
-else:
-    select_statement = "select name from names where"
-
-    for i in str(choice):
-        if len(choices) != 1:
-            select_statement += " " + m[int(i)-1]
-            select_statement += " " +"and"
-            choices.remove(int(i))
-        else:
-            select_statement += " " + m[int(i)-1]
-    cursor.execute(select_statement)
-    names = cursor.fetchall()
-    connections.commit()
-    for i in names:
-        print(i[0])
-
+data  = []
+select_statement = "select * from names where"
+for i in str(choice):
+    if len(choices) != 1:
+        select_statement += " " + m[int(i)-1]
+        select_statement += " " +"and"
+        choices.remove(int(i))
+    else:
+        select_statement += " " + m[int(i)-1]
+cursor.execute(select_statement)
+names = cursor.fetchall()
+connections.commit()
+for i in names:
+    name = [i[1],i[3]]
+    data.append(name)
+table = tabulate.tabulate(data,headers = ["Name","Category"])
+print(table)
 
